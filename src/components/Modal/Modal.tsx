566188df801ff1e3c9faad8f1ae949bbdb756082
Vlad Makhnenko <vlmakh@gmail.com>
2023-02-26 10:11:45 +0200

    Update Modal.tsx

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -4,35 +4,35 @@ import { createPortal } from 'react-dom';
 import React from 'react';
 import { ReactNode } from 'react';
 
-const modalRoot = document.querySelector('#modal-root') as HTMLElement;
+const modalRoot = document.querySelector('#modal-root') as HTMLDivElement;
 
-type PropsType = {
-  children: ReactNode,
-  onClose: () => void,
-}
+type PortalPropsType = {
+  children: ReactNode;
+  onClose: () => void;
+};
 
-export default class Modal extends Component <PropsType, {}> {
-  componentDidMount() {
+export default class Modal extends Component<PortalPropsType> {
+  componentDidMount(): void {
     window.addEventListener('keydown', this.handleEscape);
   }
 
-  componentWillUnmount() {
+  componentWillUnmount(): void {
     window.removeEventListener('keydown', this.handleEscape);
   }
 
-  handleEscape = (event: { code: string; }) => {
+  handleEscape = (event: { code: string }): void => {
     if (event.code === 'Escape') {
       this.props.onClose();
     }
   };
 
-  handleBackdrop = (event: { currentTarget: any; target: any; }) => {
+  handleBackdrop = (event: { currentTarget: any; target: any }): void => {
     if (event.currentTarget === event.target) {
       this.props.onClose();
     }
   };
 
-  render() {
+  render(): React.ReactElement<PortalPropsType> {
     return createPortal(
       <div className={css.overlay} onClick={this.handleBackdrop}>
         <div className={css.modal}>{this.props.children}</div>
